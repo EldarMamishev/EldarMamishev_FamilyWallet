13e54918f53954cfd0c1b099fb83ae223bd629ea
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-03-28 16:06:42 +0200

    Added UnitOfWork class

---
+++ Program/FamilyWallet/Data/EF/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.EF.UnitOfWork.Interface;
+using Domain.Repository;
+
+namespace Data.EF.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IFamilyRepository FamilyRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IOperationCategoryRepository OperationCategoryRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IOperationInfoRepository OperationInfoRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IOperationRepository OperationRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IPersonFamilyRepository PersonFamilyRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IPersonRepository PersonRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IPersonWalletRepository PersonWalletRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ITransactionRepository TransactionRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IWalletRepository WalletRepository { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
