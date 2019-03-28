b5f006d93b9e1c26e557537e93141ebd1c9bcd74
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-03-28 16:04:27 +0200

    added DbSets

--- Program/FamilyWallet/Data/EF/FamilyWalletContext.cs
+++ Program/FamilyWallet/Data/EF/FamilyWalletContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Domain.Entity;
 
 namespace Data.EF
 {
@@ -12,5 +13,15 @@ namespace Data.EF
 
         public FamilyWalletContext(DbContextOptions options) : base(options)
         { }
+
+        public DbSet<Family> Families { get; }
+        public DbSet<Operation> Operations { get; }
+        public DbSet<OperationCategory> OperationCategories { get; }
+        public DbSet<OperationInfo> OperationInfos { get; }
+        public DbSet<Person> People { get; }
+        public DbSet<PersonFamily> PersonFamilies { get; }
+        public DbSet<PersonWallet> PersonWallets { get; }
+        public DbSet<Transaction> Transactions { get; }
+        public DbSet<Wallet> Wallets { get; }
     }
 }
