f5dc945710f908ab5e09ec9ca3845cf837fd5036
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-03-28 15:59:12 +0200

    added UnitOfWork

---
+++ Program/FamilyWallet/Data/EF/UnitOfWork/Interface/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Repository;
+
+namespace Data.EF.UnitOfWork.Interface
+{
+    public interface IUnitOfWork
+    {
+        IFamilyRepository FamilyRepository { get; set; }
+        IOperationCategoryRepository OperationCategoryRepository { get; set; }
+        IOperationInfoRepository OperationInfoRepository { get; set; }
+        IOperationRepository OperationRepository { get; set; }
+        IPersonFamilyRepository PersonFamilyRepository { get; set; }
+        IPersonRepository PersonRepository { get; set; }
+        IPersonWalletRepository PersonWalletRepository { get; set; }
+        ITransactionRepository TransactionRepository { get; set; }
+        IWalletRepository WalletRepository { get; set; }
+    }
+}
