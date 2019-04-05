2752b47e821e913a7ba93d85617bba708e73dc37
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-04-05 13:02:57 +0300

    Added ViewModels

---
+++ Program/FamilyWallet/Services/ViewModel/PersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Services.ViewModel.Base;
+
+namespace Services.ViewModel
+{
+    public class PersonViewModel : EntityViewModelBase
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public SocialViewModel SocialViewModel { get; set; }
+        public IEnumerable<PersonWalletViewModel> PersonWallets { get; set; }
+        public IEnumerable<FamilyViewModel> Families { get; set; }
+    }
+}
---
+++ Program/FamilyWallet/Services/ViewModel/WalletViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entity;
+using Domain.Enum;
+using Services.ViewModel.Base;
+
+namespace Services.ViewModel
+{
+    public class WalletViewModel : EntityViewModelBase
+    {
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public WalletType WalletType { get; set; }
+        public Family Family { get; set; }
+        public IEnumerable<PersonWalletViewModel> PersonWallets { get; set; }
+    }
+}
