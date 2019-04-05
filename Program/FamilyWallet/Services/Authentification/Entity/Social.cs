ed44481863115dee3eea3489734c74686a182c8b
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-04-05 13:35:37 +0300

    added social entity

--- Program/FamilyWallet/Services/Authentification/Entity/Social.cs
+++ Program/FamilyWallet/Services/Authentification/Entity/Social.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Services.Authentification.Enum;
 
 namespace Services.Authentification.Entity
 {
     public class Social : IdentityUser
     {
-
+        public int PersonId { get; set; }
+        public SocialMedia SocialMedia { get; set; }
+        public int SocialId { get; set; }
     }
 }
