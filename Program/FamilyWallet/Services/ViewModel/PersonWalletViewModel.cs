2cc2feb1bb43dccf6533bb15350f5584a8d00eca
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-04-12 10:24:32 +0300

    removed inheritance from not existing base class

--- Program/FamilyWallet/Services/ViewModel/PersonWalletViewModel.cs
+++ Program/FamilyWallet/Services/ViewModel/PersonWalletViewModel.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain.Enum;
-using Services.ViewModel.Base;
 
 namespace Services.ViewModel
 {
-    public class PersonWalletViewModel : EntityViewModelBase
+    public class PersonWalletViewModel
     {
         public PersonViewModel Person { get; set; }
         public FamilyWithPeopleViewModel Family { get; set; }
