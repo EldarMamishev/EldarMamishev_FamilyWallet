1e37d0c92a5cc2171ac4de02032866adc55e9baa
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-04-15 10:56:48 +0300

    changed folder structure, added family-module, service and routing, removed component

--- Program/FamilyWallet/View/src/app/app-routing.module.ts
+++ Program/FamilyWallet/View/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FamilyComponent } from './components/family/family.component';
 import { DefaultConstants } from './constants/default-constants';
+import { FamiliesByPersonComponent } from './family/families-by-person/families-by-person.component';
 
 const routes: Routes = [
-  { path: DefaultConstants.CONNECTION_PATH + '/family', component: FamilyComponent }
+  { path: DefaultConstants.CONNECTION_PATH + '/family', component: FamiliesByPersonComponent }
 ];
 
 @NgModule({
--- Program/FamilyWallet/View/src/app/app.module.ts
+++ Program/FamilyWallet/View/src/app/app.module.ts
@@ -5,17 +5,15 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FamilyComponent } from './components/family/family.component';
-import { FamilyWithPeopleComponent } from './components/family-components/family-with-people/family-with-people.component';
-import { AddPersonToFamilyComponent } from './components/family-components/add-person-to-family/add-person-to-family.component';
-import { UpdateFamilyComponent } from './components/family-components/update-family/update-family.component';
-import { CreateFamilyComponent } from './components/family-components/create-family/create-family.component';
-import { FamiliesByPersonComponent } from './components/family-components/families-by-person/families-by-person.component';
+import { FamilyWithPeopleComponent } from './family/family-with-people/family-with-people.component';
+import { AddPersonToFamilyComponent } from './family/add-person-to-family/add-person-to-family.component';
+import { UpdateFamilyComponent } from './family/update-family/update-family.component';
+import { CreateFamilyComponent } from './family/create-family/create-family.component';
+import { FamiliesByPersonComponent } from './family/families-by-person/families-by-person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FamilyComponent,
     FamilyWithPeopleComponent,
     AddPersonToFamilyComponent,
     UpdateFamilyComponent,
