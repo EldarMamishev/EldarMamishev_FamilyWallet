8f09cf1db10ed110d64905b5fa4e3fc7bb96b6b5
eldar.mamishev <eldar.mamishev@sigmaukraine.com>
2019-04-15 16:13:28 +0300

    changed routing

--- Program/FamilyWallet/View/src/app/app-routing.module.ts
+++ Program/FamilyWallet/View/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultConstants } from './constants/default-constants';
 import { FamiliesByPersonComponent } from './family/families-by-person/families-by-person.component';
+import { FamilyModule } from './family/family.module';
 
 const routes: Routes = [
-  { path: DefaultConstants.CONNECTION_PATH + '/family', component: FamiliesByPersonComponent }
+  { path: '', redirectTo: 'family', pathMatch: 'full' },
+ 
+  { path: 'family', loadChildren: () => FamilyModule },    
 ];
 
 @NgModule({
--- Program/FamilyWallet/View/src/app/family/family-routing.module.ts
+++ Program/FamilyWallet/View/src/app/family/family-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultConstants } from '../constants/default-constants';
+import { FamiliesByPersonComponent } from './families-by-person/families-by-person.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'person', pathMatch: 'full' },
+
+  { path: 'person', component: FamiliesByPersonComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class FamilyRoutingModule { }
